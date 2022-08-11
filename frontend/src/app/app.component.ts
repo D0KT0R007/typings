6a902a74613217862eb4d1bf88deefae3716a17c
Hannes.Klein <hannes.klein@4k-analytics.de>
2022-08-11 10:04:04 +0200

    First Working Alpha

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  punkte =0
+  toWrite = 'Hellooo';
+  @Input()
+  written! :string;
+
+  async wordWritten() {
+    if (this.toWrite===this.written){
+      this.toWrite="YES"
+      this.written=""
+      this.punkte++
+    }
+
+  }
 }
